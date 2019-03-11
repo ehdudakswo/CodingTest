80c13c68ce40f55ee477f9e44c532bfa51515840
정도영 <ehdudakswo@gmail.com>
2019-03-11 17:51:16 +0900

    Bubble Sort

---
+++ app/src/test/java/com/example/myapplication/Sort.java
@@ -0,0 +1,22 @@
+package com.example.myapplication;
+
+public class Sort {
+
+    public int[] bubble(int[] inputs) {
+        int[] arr = inputs.clone();
+        int len = arr.length;
+
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - 1 - i; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int temp = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
+                }
+            }
+        }
+
+        return arr;
+    }
+
+}
---
+++ app/src/test/java/com/example/myapplication/SortTest.java
@@ -0,0 +1,21 @@
+package com.example.myapplication;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class SortTest {
+
+    private int[] arr = {9, 7, 5, 3, 1};
+
+    @Test
+    public void testBubble() {
+        int[] sorted = new Sort().bubble(arr);
+        assertEquals(1, sorted[0]);
+        assertEquals(3, sorted[1]);
+        assertEquals(5, sorted[2]);
+        assertEquals(7, sorted[3]);
+        assertEquals(9, sorted[4]);
+    }
+
+}
